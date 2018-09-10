9d02f4f984d1d1a0e824ffafc1ced8d948d968b3
AmithLiyanage <amith.liyanage.9@gmail.com>
2018-09-10 16:18:21 +0530

    fourth commit

--- app.js
+++ app.js
@@ -34,10 +34,19 @@ const data = {
 // }); 
 
 // Update Post
-http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
-  if(err){
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log(post);
+//   }  
+// });
+
+// Delet Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if(err) {
     console.log(err);
-  }else{
-    console.log(post);
-  }  
+  } else {
+    console.log(response);
+  }
 });
