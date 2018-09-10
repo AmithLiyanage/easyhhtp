56814117f94e281d063158ade9dee13df2f6ad97
AmithLiyanage <amith.liyanage.9@gmail.com>
2018-09-10 15:57:49 +0530

    third commit

--- easyhttp.js
+++ easyhttp.js
@@ -9,7 +9,7 @@ easyHTTP.prototype.get = function(url, callback) {
   let self = this;
   this.http.onload = function() {
     if(self.http.status === 200){
-      callback(null, self.http.responseTex);
+      callback(null, self.http.responseText);
     } else {
       callback('Error: ' + self.http.status);
     }
@@ -23,9 +23,25 @@ easyHTTP.prototype.post = function(url, data, callback) {
   this.http.open('POST', url, true);
   this.http.setRequestHeader('Content-type', 'application/json');
 
-  this.send(JSON.stringify(data));
+  let self = this;
+  this.http.onload = function() {
+    callback(null, self.http.responseText);
+  }
+
+  this.http.send(JSON.stringify(data));
 }
 
 // Make an HTTP PUT Request
+easyHTTP.prototype.put = function(url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function() {
+    callback(null, self.http.responseText);
+  }
+
+  this.http.send(JSON.stringify(data));
+}
 
 // Make an HTTP DELETE Request
